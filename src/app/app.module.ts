93df5eeecff8bddf8b4dbd94ebda3a2d129c1a44
Douglas <contatodouglasfabiano1@gmail.com>
2025-02-19 15:02:01 -0300

    custom error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,16 @@ import { MatInputModule } from '@angular/material/input';
 import { EmailValidatorDirective } from './components/ex2/directives/email-validator.directive';
 import { Ex1Component } from './components/ex1/ex1.component';
 import { Ex2Component } from './components/ex2/ex2.component';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { CustomErrorStateMatcher } from './components/ex2/utils/custom-error-state-matcher';
 
 @NgModule({
-  declarations: [AppComponent, EmailValidatorDirective, Ex1Component, Ex2Component],
+  declarations: [
+    AppComponent,
+    EmailValidatorDirective,
+    Ex1Component,
+    Ex2Component,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -20,7 +27,13 @@ import { Ex2Component } from './components/ex2/ex2.component';
     MatFormFieldModule,
     MatInputModule,
   ],
-  providers: [provideAnimationsAsync()],
+  providers: [
+    {
+      provide: ErrorStateMatcher,
+      useClass: CustomErrorStateMatcher,
+    },
+    provideAnimationsAsync(),
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
